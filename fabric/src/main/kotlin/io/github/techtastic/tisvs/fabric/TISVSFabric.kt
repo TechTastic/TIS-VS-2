31be0814fc315c6bdbdbebb7d29633fd6865d0f0
TechTastic <74630543+TechTastic@users.noreply.github.com>
2024-10-26 11:09:38 -0500

    Added recipes and Logo

--- fabric/src/main/kotlin/io/github/techtastic/tisvs/fabric/TISVSFabric.kt
+++ fabric/src/main/kotlin/io/github/techtastic/tisvs/fabric/TISVSFabric.kt
@@ -2,10 +2,13 @@ package io.github.techtastic.tisvs.fabric
 
 import io.github.techtastic.tisvs.TISVS.init
 import io.github.techtastic.tisvs.TISVS.initClient
+import io.github.techtastic.tisvs.module.TISVSModules
+import li.cil.tis3d.common.item.ModCreativeTabs
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import org.valkyrienskies.mod.fabric.common.ValkyrienSkiesModFabric
 
 object TISVSFabric: ModInitializer {
@@ -14,6 +17,14 @@ object TISVSFabric: ModInitializer {
         ValkyrienSkiesModFabric().onInitialize()
 
         init()
+        
+        ItemGroupEvents.modifyEntriesEvent(ModCreativeTabs.COMMON.key).register { entries ->
+            entries.accept(TISVSModules.ALTITUDE_ITEM.get())
+            entries.accept(TISVSModules.DISTANCE_ITEM.get())
+            entries.accept(TISVSModules.GYROSCOPIC_ITEM.get())
+            entries.accept(TISVSModules.OMEGA_ITEM.get())
+            entries.accept(TISVSModules.VELOCITY_ITEM.get())
+        }
     }
 
     @Environment(EnvType.CLIENT)
--- forge/src/main/kotlin/io/github/techtastic/tisvs/forge/TISVSForge.kt
+++ forge/src/main/kotlin/io/github/techtastic/tisvs/forge/TISVSForge.kt
@@ -9,6 +9,9 @@ import io.github.techtastic.tisvs.TISVS.initClient
 import io.github.techtastic.tisvs.manual.TISVSManual
 import io.github.techtastic.tisvs.module.TISVSModules
 import io.github.techtastic.tisvs.serial.TISVSSerialInterfaces
+import li.cil.tis3d.common.item.ModCreativeTabs
+import net.minecraftforge.common.CreativeModeTabRegistry
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent
 import thedarkcolour.kotlinforforge.KotlinModLoadingContext
 
 @Mod(MOD_ID)
@@ -17,11 +20,8 @@ class TISVSForge {
         val bus = KotlinModLoadingContext.get().getKEventBus()
         EventBuses.registerModEventBus(MOD_ID, bus)
 
-        bus.addListener { event: FMLClientSetupEvent? ->
-            clientSetup(
-                event
-            )
-        }
+        bus.addListener(this::clientSetup)
+        bus.addListener(this::onTabRegistry)
 
         TISVSModules.registerModules()
         TISVSSerialInterfaces.register()
@@ -33,7 +33,17 @@ class TISVSForge {
         init()
     }
 
-    private fun clientSetup(event: FMLClientSetupEvent?) {
+    private fun clientSetup(event: FMLClientSetupEvent) {
         initClient()
+    }
+
+    private fun onTabRegistry(event: BuildCreativeModeTabContentsEvent) {
+        if (event.tab == ModCreativeTabs.COMMON.get()) {
+            event.accept(TISVSModules.ALTITUDE_ITEM)
+            event.accept(TISVSModules.DISTANCE_ITEM)
+            event.accept(TISVSModules.GYROSCOPIC_ITEM)
+            event.accept(TISVSModules.OMEGA_ITEM)
+            event.accept(TISVSModules.VELOCITY_ITEM)
+        }
     }
 }
